294b05d1bd003e9d4f76559071fe673af55f436c
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2025-01-26 00:31:36 +0530

    Remove placeholder text from TimelineDemo component

--- location-zkp-next/src/app/components/TimeLine.jsx
+++ location-zkp-next/src/app/components/TimeLine.jsx
@@ -9,9 +9,7 @@ export function TimelineDemo() {
       title: "2024",
       content: (
         <div>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Built and launched Aceternity UI and Aceternity UI Pro from scratch
-          </p>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8"></p>
           <div className="grid grid-cols-2 gap-4">
             <Link href={`/dashboard/hackathon/${1}`}>
               <Image
@@ -57,14 +55,6 @@ export function TimelineDemo() {
       title: "2023",
       content: (
         <div>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            I usually run out of copy, but when I see content this big, I try to
-            integrate lorem ipsum.
-          </p>
-          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-            Lorem ipsum is for people who are too lazy to write copy. But we are
-            not. Here are some more example of beautiful designs I built.
-          </p>
           <div className="grid grid-cols-2 gap-4">
             <Link href={`/dashboard/hackathon/${5}`}>
               <Image
