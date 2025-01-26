2ad696133affd4f7836803456436d17f18cae7ea
DengreSarthak <dengresathak2002@gmail.com>
2025-01-26 08:11:17 +0530

    changes

--- location-zkp-next/src/components/Hero.jsx
+++ location-zkp-next/src/components/Hero.jsx
@@ -73,22 +73,21 @@ export const Hero = () => {
       ></motion.div>
 
       <div className="container relative text-center">
-        {/* <p className="font-caudex text-4xl max-w-xl mx-auto text-blue-300 mb-5 tracking-tight italic">
-          ✨ Empowering the decentralization of asset ownership through
-          cryptographically secure, privacy-first NFT frameworks ✨
-        </p> */}
+        <p className="font-caudex text-4xl max-w-xl mx-auto text-blue-300 mb-5 tracking-tight italic">
+          ✨ Privacy-preserving NFTs for a connected world ✨
+        </p>
         <div className="flex items-center justify-center space-x-4">
           <h1 className="font-caudex text-10xl md:text-[180px] md:leading-none tracking-tighter bg-gradient-to-br from-[#60A5FA] via-[#3B82F6] to-[#2563EB] text-transparent bg-clip-text pb-4">
             Nexus
           </h1>
         </div>
-        <p className="font-caudex text-3xl max-w-2xl mx-auto text-blue-400 mt-5 tracking-tight">
-          Seamlessly mint NFTs tethered to geospatial parameters,
-          fortified by advanced zero-knowledge proof methodologies
+        <p className="font-caudex text-4xl max-w-2xl mx-auto text-blue-400 mt-5 tracking-tight">
+          Mint NFTs securely based on your location, powered by zero-knowledge
+          proofs
         </p>
         <div className="flex justify-center mt-7">
           <Link href="/home">
-            <Button>Delve into Nexus</Button>
+            <Button>Explore Nexus</Button>
           </Link>
         </div>
       </div>
